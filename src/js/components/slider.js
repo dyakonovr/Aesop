a5d78cf9fa3cb464168f9923703333faa58201e4
dyakonovr <dyakonov.rr@bk.ru>
2021-11-11 14:57:52 +0400

    Сделал 2 секции со слайдером, 2 секции с информацией и секцию с цитатой

---
+++ src/js/components/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: false,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    hide: false,
+    snapOnRelease: true,
+  },
+});
