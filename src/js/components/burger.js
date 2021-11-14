fd71db26b294a93d010e7fab867a65b7aedc7a6a
dyakonovr <dyakonov.rr@bk.ru>
2021-11-14 13:38:08 +0400

    Сделал меню-бургер, небольшие доработки

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,7 +1,19 @@
+const header = document.querySelector('.header');
+const menuBlock = document.querySelector('.header__menu');
 const burger = document.querySelector('.header__burger');
+const nav = document.querySelector('.header__nav');
+const noticeBlocks = document.querySelectorAll('.notice');
 
-if (burger) {
+if (burger && menuBlock && nav) {
   burger.addEventListener('click', function () {
-    this.classList.toggle('header__burger--active')
+    for (let i = 0; i < noticeBlocks.length; i++) {
+      let notice = noticeBlocks[i];
+      toggle(notice);
+    }
+    scrollToggle();
+    this.classList.toggle('header__burger--active');
+    header.classList.toggle('header--active');
+    menuBlock.classList.toggle('header__menu--active');
+    nav.classList.toggle('header__nav--active');
   });
 }
